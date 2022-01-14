dac9cc3ca773148232afa8e46bbd321c2e730660
Utku Cakir <utkucakirr@gmail.com>
2022-01-14 16:38:32 +0300

    Add project files.

---
+++ UtkuCakir_Odev2/Startup.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core;
+using Core.MiddleWare;
+using Data.Context;
+using Data.Uow;
+using Microsoft.EntityFrameworkCore;
+
+namespace UtkuCakir_Odev2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //add context
+            var dbConfig = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<VehicleDbContext>(options =>
+                options.UseSqlServer("Server=LAPTOP-58K1GN78; Database=HW2DB;Trusted_Connection=True;"));
+
+            //mapper
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            services.AddSingleton(mapperConfig.CreateMapper());
+
+            //add uow
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UtkuCakir_Odev2", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UtkuCakir_Odev2 v1"));
+            }
+
+            //middleware
+            app.UseMiddleware<HeartBeatMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
